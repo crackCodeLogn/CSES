76b7365238d4a184bfb16e49823a81d55de95dcb
Vivek <vivektheintel@gmail.com>
2022-01-11 23:56:50 +0530

    Adding Set traversal

--- Template.cpp
+++ Template.cpp
@@ -11,7 +11,7 @@ typedef pair<int, int> pi;
 #define MP make_pair
 #define REP(i,a,b) for(int i=a;i<=b;i++)
 #define REP_MAP(it,dataMap) for(auto it = dataMap.cbegin(); it != dataMap.cend(); ++it)
-
+#define REP_SET(it,dataSet) for(auto it = dataSet.begin(); it != dataSet.end(); ++it)
 
 int main() {
     ios::sync_with_stdio(0);
